697d9f424fb87dee962d62fab25b0ff252b8fcd9
Kanishk Raheja <147127794+kanishk-raheja@users.noreply.github.com>
2025-01-13 21:42:28 +0530

    Unit1.3.java

---
+++ Unit1.3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+      // Taking String As an input
+        System.out.println("Welcome, "+ s +"!");
+      // + is used to add various Strings for Printing
+    }
+}
