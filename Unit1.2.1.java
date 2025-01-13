b09b7ba5ae4afb278031b0ffb79c3d214327a8ac
Kanishk Raheja <147127794+kanishk-raheja@users.noreply.github.com>
2025-01-13 21:54:51 +0530

    Unit1.2.1.java

---
+++ Unit1.2.1.java
@@ -0,0 +1,12 @@
+public static void main(String args[]) {  
+        //Type your code here
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c;
+        c=a;
+        a=b;
+        b=c;
+        // Replace value
+        System.out.println(a+" "+b);
+    }  
