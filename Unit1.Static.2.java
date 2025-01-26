48cef9324e87aa2cd70bb47bf04fa98761634ff0
Kanishk Raheja <147127794+kanishk-raheja@users.noreply.github.com>
2025-01-26 21:13:18 +0530

    Unit1.Static.2.java

---
+++ Unit1.Static.2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+s
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        int result = ((a + b - c * d / a) % b);
+
+        System.out.println("(a + b - c * d / a) % b = " + result);
+
+    }
+}
