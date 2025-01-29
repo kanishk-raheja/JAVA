275208ebf75b864644e64542acf0173a82a2e1b0
Kanishk Raheja <147127794+kanishk-raheja@users.noreply.github.com>
2025-01-29 14:52:44 +0530

    Unit1.Static.2.java

--- Unit1.Static.2.java
+++ Unit1.Static.2.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-s
+
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int c = scanner.nextInt();
