e76f50395a9fc07464a60d6d780657e2ea571886
Kanishk Raheja <147127794+kanishk-raheja@users.noreply.github.com>
2025-01-29 14:44:05 +0530

    Unit1.2.3.java

--- Unit1.2.3.java
+++ Unit1.2.3.java
@@ -3,6 +3,7 @@ public class Main
   {
     public static void main(String[] args) {
         //Type your code here
+        Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
